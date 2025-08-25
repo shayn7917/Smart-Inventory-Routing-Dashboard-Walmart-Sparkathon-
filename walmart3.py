2df2bb310be0505f5250cc3a01ea354bd383c04a
Shishir Mukherjee <144459996+Shi1709@users.noreply.github.com>
2025-08-26 02:57:51 +0530

    Update walmart3.py

--- walmart3.py
+++ walmart3.py
@@ -41,8 +41,8 @@ st.title("🚚 Walmart Smart Inventory Routing System")
 st.subheader("Optimized Product Transfers Between Supercenters Based on Real-Time Conditions")
 
 # API Keys (Replace with your actual keys)
-VISUALCROSSING_API_KEY = "84L6CRMYK6LDSYJDLDBL7SG8V"
-GOOGLE_MAPS_API_KEY = "AIzaSyARLmytrjxK2cZsrXh2-2TUFBjiANnrkiE" 
+VISUALCROSSING_API_KEY = "Your_VISUALCROSSING_API_KEY"
+GOOGLE_MAPS_API_KEY = "Your_GOOGLE_MAPS_API_KEY" 
 gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
 
 # Constants
